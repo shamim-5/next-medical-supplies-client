8b5e776f33ab24c63e692ab75fdde75fb1200a73
shamim-5 <shamim.somadder5@gmail.com>
2023-10-19 18:04:08 +0600

    handle protected route with redux state

--- src/components/AuthCheck.tsx
+++ src/components/AuthCheck.tsx
@@ -1,9 +1,22 @@
 "use client";
 
 import useAuthCheck from "@/redux/hooks/useAuthCheck";
+import { Alert, Spin } from "antd";
 
 export default function AuthCheck({ children }: { children: React.ReactNode }) {
   const authCheck = useAuthCheck();
 
-  return !authCheck ? <div>Checking Authentication.....</div> : <>{children}</>;
+  return !authCheck ? (
+    <div className="flex flex-col items-center justify-center my-12">
+      <Spin tip="Loading..." size="large">
+        <Alert className="h-32 mb-0 pb-0"
+          message="Checking Authentication..."
+          description="Please wait...checking authentication is in progress!"
+          type="info"
+        />
+      </Spin>
+    </div>
+  ) : (
+    <>{children}</>
+  );
 }
